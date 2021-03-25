6a299f879be84a21db0e2789e712cfd9e1e65400
francescostag <s278937@studenti.polito.it>
2021-03-25 18:20:03 +0100

    Save button created

--- app/src/main/java/com/example/madproject/EditProfileActivity.kt
+++ app/src/main/java/com/example/madproject/EditProfileActivity.kt
@@ -1,12 +1,53 @@
 package com.example.madproject
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.KeyListener
+import android.view.*
+import android.widget.*
+
+var t: Toast? = null
+
+fun setToast(message: String?, context: Context) {
+    t?.cancel()
+    t = Toast.makeText(context, message, Toast.LENGTH_LONG)
+    t?.show()
+}
 
 class EditProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
-        TODO("Create the application")
+        this.setTitle("Edit your profile...")
+
+        val editText = findViewById<EditText>(R.id.edit)
+
+        editText.setOnFocusChangeListener { v, hasFocus ->
+            if (!hasFocus) {
+                editText.setSelection(0, 0)
+            }
+        }
+
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.save_profile, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        return when (item.itemId) {
+            R.id.saveButton -> {
+                val text = findViewById<TextView>(R.id.tv)
+                setToast("Saving...", text.context)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/madproject/ShowProfileActivity.kt
+++ app/src/main/java/com/example/madproject/ShowProfileActivity.kt
@@ -1,11 +1,20 @@
 package com.example.madproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class ShowProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        this.setTitle("")
+
+        val actButton = findViewById<Button>(R.id.button)
+        actButton.setOnClickListener {
+            val intent = Intent(this, EditProfileActivity::class.java)
+            startActivityForResult(intent, 1)
+        }
     }
 }

096efd8d66bde24de38bc6d8580c13b1ae561fe9
Atabay Heydarli <heydarli.atabay@gmail.com>
2021-03-25 09:40:39 +0100

    First commit

---
+++ app/src/main/java/com/example/madproject/ShowProfileActivity.kt
@@ -0,0 +1,11 @@
+package com.example.madproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShowProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

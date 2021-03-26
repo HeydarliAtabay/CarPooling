16c7be67e62b7780ce4ff6ef11d9f22fc1555f95
Atabay Heydarli <heydarli.atabay@gmail.com>
2021-03-26 16:16:50 +0100

    First task for show profile

--- app/src/main/java/com/example/madproject/EditProfileActivity.kt
+++ app/src/main/java/com/example/madproject/EditProfileActivity.kt
@@ -7,70 +7,15 @@ import android.text.method.KeyListener
 import android.view.*
 import android.widget.*
 
-var t: Toast? = null
 
-fun setToast(message: String?, context: Context) {
-    t?.cancel()
-    t = Toast.makeText(context, message, Toast.LENGTH_LONG)
-    t?.show()
-}
 
 class EditProfileActivity : AppCompatActivity() {
-    private lateinit var fullName:EditText
-    private lateinit var nickName:EditText
-    private lateinit var email:EditText
-    private lateinit var location:EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
         this.setTitle("Edit your profile...")
 
-        fullName = findViewById(R.id.fullName)
-        nickName = findViewById(R.id.nickName)
-        email = findViewById(R.id.email)
-        location = findViewById(R.id.location)
-
-
-        fullName.setOnFocusChangeListener { v, hasFocus ->
-            if (!hasFocus) {  // lost focus
-                fullName.setSelection(0,0)
-            } }
-
-        nickName.setOnFocusChangeListener { v, hasFocus ->
-            if (!hasFocus) {  // lost focus
-                nickName.setSelection(0,0)
-            } }
-
-        email.setOnFocusChangeListener { v, hasFocus ->
-            if (!hasFocus) {  // lost focus
-                email.setSelection(0,0)
-            } }
-
-        location.setOnFocusChangeListener { v, hasFocus ->
-            if (!hasFocus) {  // lost focus
-                location.setSelection(0,0)
-            } }
-
 
     }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        val inflater: MenuInflater = menuInflater
-        inflater.inflate(R.menu.save_profile, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle item selection
-        return when (item.itemId) {
-            R.id.saveButton -> {
-                val text = findViewById<TextView>(R.id.fullName)
-                setToast("Saving...", text.context)
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
-
 }
--- app/src/main/java/com/example/madproject/ShowProfileActivity.kt
+++ app/src/main/java/com/example/madproject/ShowProfileActivity.kt
@@ -3,18 +3,29 @@ package com.example.madproject
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+
 import android.widget.Button
+
+import android.widget.TextView
+
 
 class ShowProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         this.setTitle("")
+        setContentView(R.layout.activity_show_profile)
+        val fullname="Heydarli Atabay"
+        val nickname="Atash"
+        val emailaddress="heydarli.atabay@gmail.com"
+        val location="Italy,Torino"
+        val fullnameView= findViewById<TextView>(R.id.fullNameView)
+        val nicknameView=findViewById<TextView>(R.id.nicknameView)
+        val emailaddressView=findViewById<TextView>(R.id.emailaddressView)
+        val locationView= findViewById<TextView>(R.id.LocationView)
+        fullnameView.text=fullname
+        nicknameView.text=nickname
+        emailaddressView.text=emailaddress
+        locationView.text=location
 
-        val actButton = findViewById<Button>(R.id.button)
-        actButton.setOnClickListener {
-            val intent = Intent(this, EditProfileActivity::class.java)
-            startActivityForResult(intent, 1)
-        }
     }
 }

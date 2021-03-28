56a679717d0e15dbbb6eba0fe68c1f1367d4f01b
albysola <albertosolavagione30@gmail.com>
2021-03-28 20:10:11 +0200

    Camera interaction update

--- app/src/main/java/com/example/madproject/EditProfileActivity.kt
+++ app/src/main/java/com/example/madproject/EditProfileActivity.kt
@@ -1,16 +1,26 @@
 package com.example.madproject
 
-import android.app.Activity
-import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.BitmapFactory
+import android.graphics.Matrix
+import android.media.ExifInterface
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
-import android.text.method.KeyListener
 import android.view.*
 import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
+import java.io.File
+import java.io.IOException
+import java.io.InputStream
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 var t: Toast? = null
 
@@ -40,34 +50,50 @@ class EditProfileActivity : AppCompatActivity() {
 
         fullName.setOnFocusChangeListener { v, hasFocus ->
             if (!hasFocus) {  // lost focus
-                fullName.setSelection(0,0)
+                fullName.setSelection(0, 0)
             } }
 
         nickName.setOnFocusChangeListener { v, hasFocus ->
             if (!hasFocus) {  // lost focus
-                nickName.setSelection(0,0)
+                nickName.setSelection(0, 0)
             } }
 
         email.setOnFocusChangeListener { v, hasFocus ->
             if (!hasFocus) {  // lost focus
-                email.setSelection(0,0)
+                email.setSelection(0, 0)
             } }
 
         location.setOnFocusChangeListener { v, hasFocus ->
             if (!hasFocus) {  // lost focus
-                location.setSelection(0,0)
+                location.setSelection(0, 0)
             } }
 
         val button = findViewById<ImageButton>(R.id.imageButton)
         button.setOnClickListener{
             dispatchTakePictureIntent()
-        }
 
+        }
 
 
     }
 
+    lateinit var currentPhotoPath: String
 
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        // Create an image file name
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+                "JPEG_${timeStamp}_", /* prefix */
+                ".jpg", /* suffix */
+                storageDir /* directory */
+        ).apply {
+            // Save a file: path for use with ACTION_VIEW intents
+            currentPhotoPath = absolutePath
+        }
+    }
+/*
     private fun dispatchTakePictureIntent() {
         val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
         try {
@@ -76,14 +102,140 @@ class EditProfileActivity : AppCompatActivity() {
             // display error state to the user
         }
     }
+*/
+lateinit var photoURI:Uri
+private fun dispatchTakePictureIntent() {
+    Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+        // Ensure that there's a camera activity to handle the intent
+        takePictureIntent.resolveActivity(packageManager)?.also {
+            // Create the File where the photo should go
+            val photoFile: File? = try {
+                createImageFile()
+            } catch (ex: IOException) {
+                // Error occurred while creating the File
+
+                null
+            }
+            // Continue only if the File was successfully created
+            photoFile?.also {
+                photoURI = FileProvider.getUriForFile(
+                        this,
+                        "com.example.android.fileprovider",
+                        it
+                )
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                startActivityForResult(takePictureIntent, 1)
+            }
+        }
+    }
+}
+
+    private fun setPic() {
+        // Get the dimensions of the View
+        val targetW: Int = imageView.width
+        val targetH: Int = imageView.height
+
+        val bmOptions = BitmapFactory.Options().apply {
+            // Get the dimensions of the bitmap
+            inJustDecodeBounds = true
+
+            //BitmapFactory.decodeFile(currentPhotoPath, bmOptions)
+
+            val photoW: Int = outWidth
+            val photoH: Int = outHeight
+
+            // Determine how much to scale down the image
+            val scaleFactor: Int = Math.max(1, Math.min(photoW / targetW, photoH / targetH))
+
+            // Decode the image file into a Bitmap sized to fill the View
+            inJustDecodeBounds = false
+            inSampleSize = scaleFactor
+            inPurgeable = true
+
+        }
+        BitmapFactory.decodeFile(currentPhotoPath, bmOptions)?.also { bitmap ->
+            val bit = handleSamplingAndRotationBitmap(this, photoURI)
+            imageView.setImageBitmap(bit)
+        }
+    }
+
+    @Throws(IOException::class)
+    fun handleSamplingAndRotationBitmap(context: Context, selectedImage: Uri?): Bitmap? {
+        val MAX_HEIGHT = 1024
+        val MAX_WIDTH = 1024
+
+        // First decode with inJustDecodeBounds=true to check dimensions
+        val options = BitmapFactory.Options()
+        options.inJustDecodeBounds = true
+        var imageStream: InputStream? = context.contentResolver.openInputStream(selectedImage!!)
+        BitmapFactory.decodeStream(imageStream, null, options)
+        imageStream?.close()
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, MAX_WIDTH, MAX_HEIGHT)
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false
+        imageStream = context.contentResolver.openInputStream(selectedImage)
+        var img = BitmapFactory.decodeStream(imageStream, null, options)
+        img = rotateImageIfRequired(context, img, selectedImage)
+        return img
+    }
+
+    private fun calculateInSampleSize(options: BitmapFactory.Options,
+                                      reqWidth: Int, reqHeight: Int): Int {
+        // Raw height and width of image
+        val height = options.outHeight
+        val width = options.outWidth
+        var inSampleSize = 1
+        if (height > reqHeight || width > reqWidth) {
+
+            val heightRatio = Math.round(height.toFloat() / reqHeight.toFloat())
+            val widthRatio = Math.round(width.toFloat() / reqWidth.toFloat())
+
+            inSampleSize = if (heightRatio < widthRatio) heightRatio else widthRatio
+
+            val totalPixels = (width * height).toFloat()
+
+            val totalReqPixelsCap = (reqWidth * reqHeight * 2).toFloat()
+            while (totalPixels / (inSampleSize * inSampleSize) > totalReqPixelsCap) {
+                inSampleSize++
+            }
+        }
+        return inSampleSize
+    }
+
+    @Throws(IOException::class)
+    private fun rotateImageIfRequired(context: Context, img: Bitmap?, selectedImage: Uri): Bitmap? {
+        val input = context.contentResolver.openInputStream(selectedImage)
+        val ei: ExifInterface
+        ei = if (Build.VERSION.SDK_INT > 23) ExifInterface(input!!) else ExifInterface(selectedImage.path!!)
+        val orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL)
+        return when (orientation) {
+            ExifInterface.ORIENTATION_ROTATE_90 -> rotateImage(img, 90)
+            ExifInterface.ORIENTATION_ROTATE_180 -> rotateImage(img, 180)
+            ExifInterface.ORIENTATION_ROTATE_270 -> rotateImage(img, 270)
+            else -> img
+        }
+    }
+
+
+    private fun rotateImage(img: Bitmap?, degree: Int): Bitmap? {
+        val matrix = Matrix()
+        matrix.postRotate(degree.toFloat())
+        val rotatedImg = img?.let { Bitmap.createBitmap(it, 0, 0, img.width, img.height, matrix, true) }
+        img?.recycle()
+        return rotatedImg
+    }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == 1 && resultCode == RESULT_OK) {
-            val imageBitmap = data?.extras?.get("data") as Bitmap
-            imageView.setImageBitmap(imageBitmap)
+            setPic()
         }
     }
+
+
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val inflater: MenuInflater = menuInflater

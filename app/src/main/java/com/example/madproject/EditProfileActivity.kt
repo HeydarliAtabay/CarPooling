08154535de366cec94b2d981ea0bdeb6a4946a68
francescostag <s278937@studenti.polito.it>
2021-03-29 10:23:53 +0200

    FixOrientation class created

--- app/src/main/java/com/example/madproject/EditProfileActivity.kt
+++ app/src/main/java/com/example/madproject/EditProfileActivity.kt
@@ -1,16 +1,11 @@
 package com.example.madproject
 
 import android.content.*
-import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.graphics.Matrix
-import android.media.ExifInterface
 import android.net.Uri
-import android.os.Build
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
-import android.provider.MediaStore.Images
 import android.view.*
 import android.view.ContextMenu.ContextMenuInfo
 import android.widget.*
@@ -20,8 +15,15 @@ import java.io.*
 import java.text.SimpleDateFormat
 import java.util.*
 
-
 var t: Toast? = null
+
+enum class Requests(val value : Int)
+{
+    INTENT_CAPTURE_PHOTO(1);
+    companion object {
+        fun from(findValue: Int): Requests = Requests.values().first { it.value == findValue }
+    }
+}
 
 fun setToast(message: String?, context: Context) {
     t?.cancel()
@@ -35,6 +37,9 @@ class EditProfileActivity : AppCompatActivity() {
     private lateinit var email:EditText
     private lateinit var location:EditText
     private lateinit var imageView:ImageView
+    private lateinit var currentPhotoPath: String
+    private lateinit var photoURI:Uri
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
@@ -45,7 +50,6 @@ class EditProfileActivity : AppCompatActivity() {
         email = findViewById(R.id.email)
         location = findViewById(R.id.location)
         imageView = findViewById(R.id.imageView)
-
 
         fullName.setOnFocusChangeListener { v, hasFocus ->
             if (!hasFocus) {  // lost focus
@@ -73,198 +77,12 @@ class EditProfileActivity : AppCompatActivity() {
 
     }
 
-    lateinit var currentPhotoPath: String
-
-    @Throws(IOException::class)
-    private fun createImageFile(): File {
-        // Create an image file name
-        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-        return File.createTempFile(
-                "JPEG_${timeStamp}_", /* prefix */
-                ".jpg", /* suffix */
-                storageDir /* directory */
-        ).apply {
-            // Save a file: path for use with ACTION_VIEW intents
-            currentPhotoPath = absolutePath
-        }
-    }
-
-lateinit var photoURI:Uri
-private fun dispatchTakePictureIntent() {
-    Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
-        // Ensure that there's a camera activity to handle the intent
-        takePictureIntent.resolveActivity(packageManager)?.also {
-            // Create the File where the photo should go
-            val photoFile: File? = try {
-                createImageFile()
-            } catch (ex: IOException) {
-                // Error occurred while creating the File
-
-                null
-            }
-            // Continue only if the File was successfully created
-            photoFile?.also {
-                photoURI = FileProvider.getUriForFile(
-                        this,
-                        "com.example.android.fileprovider",
-                        it
-                )
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
-                startActivityForResult(takePictureIntent, 1)
-            }
-        }
-    }
-}
-/* Save image into the phone Gallery
-    private fun insertImage(cr: ContentResolver,
-                            source: Bitmap?,
-                            title: String?,
-                            description: String?): String? {
-        val values = ContentValues()
-        values.put(Images.Media.TITLE, title)
-        values.put(Images.Media.DISPLAY_NAME, title)
-        values.put(Images.Media.DESCRIPTION, description)
-        values.put(Images.Media.MIME_TYPE, "image/jpeg")
-        // Add the date meta data to ensure the image is added at the front of the gallery
-        values.put(Images.Media.DATE_ADDED, System.currentTimeMillis())
-        values.put(Images.Media.DATE_TAKEN, System.currentTimeMillis())
-        var url: Uri? = null
-        var stringUrl: String? = null /* value to be returned */
-        try {
-            url = cr.insert(Images.Media.EXTERNAL_CONTENT_URI, values)
-            if (source != null) {
-                val imageOut: OutputStream? = cr.openOutputStream(url!!)
-                try {
-                    source.compress(Bitmap.CompressFormat.JPEG, 50, imageOut)
-                } finally {
-                    imageOut?.close()
-                }
-                val id = ContentUris.parseId(url!!)
-                // Wait until MINI_KIND thumbnail is generated.
-
-            } else {
-                cr.delete(url!!, null, null)
-                url = null
-            }
-        } catch (e: Exception) {
-            if (url != null) {
-                cr.delete(url, null, null)
-                url = null
-            }
-        }
-        if (url != null) {
-            stringUrl = url.toString()
-        }
-        return stringUrl
-    }
-
-*/
-    private fun setPic() {
-        // Get the dimensions of the View
-        val targetW: Int = imageView.width
-        val targetH: Int = imageView.height
-
-        val bmOptions = BitmapFactory.Options().apply {
-            // Get the dimensions of the bitmap
-            inJustDecodeBounds = true
-
-            //BitmapFactory.decodeFile(currentPhotoPath, bmOptions)
-
-            val photoW: Int = outWidth
-            val photoH: Int = outHeight
-
-            // Determine how much to scale down the image
-            val scaleFactor: Int = Math.max(1, Math.min(photoW / targetW, photoH / targetH))
-
-            // Decode the image file into a Bitmap sized to fill the View
-            inJustDecodeBounds = false
-            inSampleSize = scaleFactor
-            inPurgeable = true
-
-        }
-        BitmapFactory.decodeFile(currentPhotoPath, bmOptions)?.also { bitmap ->
-            val bit = handleSamplingAndRotationBitmap(this, photoURI)
-            //insertImage(contentResolver,bit,"Profile MAD","Prova")
-            imageView.setImageBitmap(bit)
-        }
-    }
-
-    @Throws(IOException::class)
-    fun handleSamplingAndRotationBitmap(context: Context, selectedImage: Uri?): Bitmap? {
-        val MAX_HEIGHT = 1024
-        val MAX_WIDTH = 1024
-
-        // First decode with inJustDecodeBounds=true to check dimensions
-        val options = BitmapFactory.Options()
-        options.inJustDecodeBounds = true
-        var imageStream: InputStream? = context.contentResolver.openInputStream(selectedImage!!)
-        BitmapFactory.decodeStream(imageStream, null, options)
-        imageStream?.close()
-
-        // Calculate inSampleSize
-        options.inSampleSize = calculateInSampleSize(options, MAX_WIDTH, MAX_HEIGHT)
-
-        // Decode bitmap with inSampleSize set
-        options.inJustDecodeBounds = false
-        imageStream = context.contentResolver.openInputStream(selectedImage)
-        var img = BitmapFactory.decodeStream(imageStream, null, options)
-        img = rotateImageIfRequired(context, img, selectedImage)
-        return img
-    }
-
-    private fun calculateInSampleSize(options: BitmapFactory.Options,
-                                      reqWidth: Int, reqHeight: Int): Int {
-        // Raw height and width of image
-        val height = options.outHeight
-        val width = options.outWidth
-        var inSampleSize = 1
-        if (height > reqHeight || width > reqWidth) {
-
-            val heightRatio = Math.round(height.toFloat() / reqHeight.toFloat())
-            val widthRatio = Math.round(width.toFloat() / reqWidth.toFloat())
-
-            inSampleSize = if (heightRatio < widthRatio) heightRatio else widthRatio
-
-            val totalPixels = (width * height).toFloat()
-
-            val totalReqPixelsCap = (reqWidth * reqHeight * 2).toFloat()
-            while (totalPixels / (inSampleSize * inSampleSize) > totalReqPixelsCap) {
-                inSampleSize++
-            }
-        }
-        return inSampleSize
-    }
-
-    @Throws(IOException::class)
-    private fun rotateImageIfRequired(context: Context, img: Bitmap?, selectedImage: Uri): Bitmap? {
-        val input = context.contentResolver.openInputStream(selectedImage)
-        val ei: ExifInterface
-        ei = if (Build.VERSION.SDK_INT > 23) ExifInterface(input!!) else ExifInterface(selectedImage.path!!)
-        val orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL)
-        return when (orientation) {
-            ExifInterface.ORIENTATION_ROTATE_90 -> rotateImage(img, 90)
-            ExifInterface.ORIENTATION_ROTATE_180 -> rotateImage(img, 180)
-            ExifInterface.ORIENTATION_ROTATE_270 -> rotateImage(img, 270)
-            else -> img
-        }
-    }
-
-
-    private fun rotateImage(img: Bitmap?, degree: Int): Bitmap? {
-        val matrix = Matrix()
-        matrix.postRotate(degree.toFloat())
-        val rotatedImg = img?.let { Bitmap.createBitmap(it, 0, 0, img.width, img.height, matrix, true) }
-        img?.recycle()
-        return rotatedImg
-    }
-
-
-
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == 1 && resultCode == RESULT_OK) {
-            setPic()
+        if (resultCode == RESULT_OK) {
+            when (requestCode) {
+                Requests.INTENT_CAPTURE_PHOTO.value -> setPic()
+            }
         }
     }
 
@@ -309,5 +127,120 @@ private fun dispatchTakePictureIntent() {
         }
     }
 
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        // Create an image file name
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+                "JPEG_${timeStamp}_", /* prefix */
+                ".jpg", /* suffix */
+                storageDir /* directory */
+        ).apply {
+            // Save a file: path for use with ACTION_VIEW intents
+            currentPhotoPath = absolutePath
+        }
+    }
+
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            // Ensure that there's a camera activity to handle the intent
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                // Create the File where the photo should go
+                val photoFile: File? = try {
+                    createImageFile()
+                } catch (ex: IOException) {
+                    // Error occurred while creating the File
+
+                    null
+                }
+                // Continue only if the File was successfully created
+                photoFile?.also {
+                    photoURI = FileProvider.getUriForFile(
+                            this,
+                            "com.example.android.fileprovider",
+                            it
+                    )
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    startActivityForResult(takePictureIntent, Requests.INTENT_CAPTURE_PHOTO.value)
+                }
+            }
+        }
+    }
+
+    /* Save image into the phone Gallery
+    private fun insertImage(cr: ContentResolver,
+                            source: Bitmap?,
+                            title: String?,
+                            description: String?): String? {
+        val values = ContentValues()
+        values.put(Images.Media.TITLE, title)
+        values.put(Images.Media.DISPLAY_NAME, title)
+        values.put(Images.Media.DESCRIPTION, description)
+        values.put(Images.Media.MIME_TYPE, "image/jpeg")
+        // Add the date meta data to ensure the image is added at the front of the gallery
+        values.put(Images.Media.DATE_ADDED, System.currentTimeMillis())
+        values.put(Images.Media.DATE_TAKEN, System.currentTimeMillis())
+        var url: Uri? = null
+        var stringUrl: String? = null /* value to be returned */
+        try {
+            url = cr.insert(Images.Media.EXTERNAL_CONTENT_URI, values)
+            if (source != null) {
+                val imageOut: OutputStream? = cr.openOutputStream(url!!)
+                try {
+                    source.compress(Bitmap.CompressFormat.JPEG, 50, imageOut)
+                } finally {
+                    imageOut?.close()
+                }
+                val id = ContentUris.parseId(url!!)
+                // Wait until MINI_KIND thumbnail is generated.
+
+            } else {
+                cr.delete(url!!, null, null)
+                url = null
+            }
+        } catch (e: Exception) {
+            if (url != null) {
+                cr.delete(url, null, null)
+                url = null
+            }
+        }
+        if (url != null) {
+            stringUrl = url.toString()
+        }
+        return stringUrl
+    }
+
+    */
+
+    private fun setPic() {
+        // Get the dimensions of the View
+        val targetW: Int = imageView.width
+        val targetH: Int = imageView.height
+
+        val bmOptions = BitmapFactory.Options().apply {
+            // Get the dimensions of the bitmap
+            inJustDecodeBounds = true
+
+            //BitmapFactory.decodeFile(currentPhotoPath, bmOptions)
+
+            val photoW: Int = outWidth
+            val photoH: Int = outHeight
+
+            // Determine how much to scale down the image
+            val scaleFactor: Int = Math.max(1, Math.min(photoW / targetW, photoH / targetH))
+
+            // Decode the image file into a Bitmap sized to fill the View
+            inJustDecodeBounds = false
+            inSampleSize = scaleFactor
+            inPurgeable = true
+
+        }
+        BitmapFactory.decodeFile(currentPhotoPath, bmOptions)?.also { bitmap ->
+            val bit = FixOrientation.handleSamplingAndRotationBitmap(this, photoURI)
+            //insertImage(contentResolver,bit,"Profile MAD","Prova")
+            imageView.setImageBitmap(bit)
+        }
+    }
 
 }
--- app/src/main/java/com/example/madproject/FixOrientation.kt
+++ app/src/main/java/com/example/madproject/FixOrientation.kt
@@ -14,54 +14,29 @@ import java.net.URI
 
 class FixOrientation {
     companion object {
-        /**
-         * This method is responsible for solving the rotation issue if exist. Also scale the images to
-         * 1024x1024 resolution
-         *
-         * @param context       The current context
-         * @param selectedImage The Image URI
-         * @return Bitmap image results
-         * @throws IOException
-         */
         @Throws(IOException::class)
-        fun handleSamplingAndRotationBitmap(context: Context, selectedImage: Uri): Bitmap? {
+        fun handleSamplingAndRotationBitmap(context: Context, selectedImage: Uri?): Bitmap? {
             val MAX_HEIGHT = 1024
             val MAX_WIDTH = 1024
 
             // First decode with inJustDecodeBounds=true to check dimensions
             val options = BitmapFactory.Options()
             options.inJustDecodeBounds = true
-            var imageStream: InputStream? = selectedImage?.let { context.getContentResolver().openInputStream(it) }
+            var imageStream: InputStream? = context.contentResolver.openInputStream(selectedImage!!)
             BitmapFactory.decodeStream(imageStream, null, options)
-            if (imageStream != null) {
-                imageStream.close()
-            }
+            imageStream?.close()
 
             // Calculate inSampleSize
             options.inSampleSize = calculateInSampleSize(options, MAX_WIDTH, MAX_HEIGHT)
 
             // Decode bitmap with inSampleSize set
             options.inJustDecodeBounds = false
-            imageStream = selectedImage?.let { context.getContentResolver().openInputStream(it) }
+            imageStream = context.contentResolver.openInputStream(selectedImage)
             var img = BitmapFactory.decodeStream(imageStream, null, options)
-            img = selectedImage?.let { img?.let { it1 -> rotateImageIfRequired(context, it1, it) } }
+            img = rotateImageIfRequired(context, img, selectedImage)
             return img
         }
 
-        /**
-         * Calculate an inSampleSize for use in a [BitmapFactory.Options] object when decoding
-         * bitmaps using the decode* methods from [BitmapFactory]. This implementation calculates
-         * the closest inSampleSize that will result in the final decoded bitmap having a width and
-         * height equal to or larger than the requested width and height. This implementation does not
-         * ensure a power of 2 is returned for inSampleSize which can be faster when decoding but
-         * results in a larger bitmap which isn't as useful for caching purposes.
-         *
-         * @param options   An options object with out* params already populated (run through a decode*
-         * method with inJustDecodeBounds==true
-         * @param reqWidth  The requested width of the resulting bitmap
-         * @param reqHeight The requested height of the resulting bitmap
-         * @return The value to be used for inSampleSize
-         */
         private fun calculateInSampleSize(options: BitmapFactory.Options,
                                           reqWidth: Int, reqHeight: Int): Int {
             // Raw height and width of image
@@ -70,22 +45,13 @@ class FixOrientation {
             var inSampleSize = 1
             if (height > reqHeight || width > reqWidth) {
 
-                // Calculate ratios of height and width to requested height and width
                 val heightRatio = Math.round(height.toFloat() / reqHeight.toFloat())
                 val widthRatio = Math.round(width.toFloat() / reqWidth.toFloat())
 
-                // Choose the smallest ratio as inSampleSize value, this will guarantee a final image
-                // with both dimensions larger than or equal to the requested height and width.
                 inSampleSize = if (heightRatio < widthRatio) heightRatio else widthRatio
 
-                // This offers some additional logic in case the image has a strange
-                // aspect ratio. For example, a panorama may have a much larger
-                // width than height. In these cases the total pixels might still
-                // end up being too large to fit comfortably in memory, so we should
-                // be more aggressive with sample down the image (=larger inSampleSize).
                 val totalPixels = (width * height).toFloat()
 
-                // Anything more than 2x the requested pixels we'll sample down further
                 val totalReqPixelsCap = (reqWidth * reqHeight * 2).toFloat()
                 while (totalPixels / (inSampleSize * inSampleSize) > totalReqPixelsCap) {
                     inSampleSize++
@@ -94,15 +60,8 @@ class FixOrientation {
             return inSampleSize
         }
 
-        /**
-         * Rotate an image if required.
-         *
-         * @param img           The image bitmap
-         * @param selectedImage Image URI
-         * @return The resulted Bitmap after manipulation
-         */
         @Throws(IOException::class)
-        private fun rotateImageIfRequired(context: Context, img: Bitmap, selectedImage: Uri): Bitmap? {
+        private fun rotateImageIfRequired(context: Context, img: Bitmap?, selectedImage: Uri): Bitmap? {
             val input = context.contentResolver.openInputStream(selectedImage)
             val ei: ExifInterface
             ei = if (Build.VERSION.SDK_INT > 23) ExifInterface(input!!) else ExifInterface(selectedImage.path!!)
@@ -115,11 +74,12 @@ class FixOrientation {
             }
         }
 
-        private fun rotateImage(img: Bitmap, degree: Int): Bitmap? {
+
+        private fun rotateImage(img: Bitmap?, degree: Int): Bitmap? {
             val matrix = Matrix()
             matrix.postRotate(degree.toFloat())
-            val rotatedImg = Bitmap.createBitmap(img, 0, 0, img.width, img.height, matrix, true)
-            img.recycle()
+            val rotatedImg = img?.let { Bitmap.createBitmap(it, 0, 0, img.width, img.height, matrix, true) }
+            img?.recycle()
             return rotatedImg
         }
     }

0e1f6a4f63041a5cac26986fbec52da3ebe1c885
francescostag <s278937@studenti.polito.it>
2021-04-02 16:03:14 +0200

    Added file Enum.kt for managing Names 2

--- app/src/main/java/com/example/madproject/ShowProfileActivity.kt
+++ app/src/main/java/com/example/madproject/ShowProfileActivity.kt
@@ -3,18 +3,72 @@ package com.example.madproject
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+
+import android.widget.ImageView
+
+import android.widget.TextView
+import android.widget.Toast
+
 
 class ShowProfileActivity : AppCompatActivity() {
+    private lateinit var fullName : TextView
+    private lateinit var nickName : TextView
+    private lateinit var dateOfBirth : TextView
+    private lateinit var email : TextView
+    private lateinit var phoneNumber : TextView
+    private lateinit var location : TextView
+    private lateinit var image : ImageView
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         this.setTitle("")
+        setContentView(R.layout.activity_show_profile)
 
-        val actButton = findViewById<Button>(R.id.button)
-        actButton.setOnClickListener {
-            val intent = Intent(this, EditProfileActivity::class.java)
-            startActivityForResult(intent, 1)
+        fullName = findViewById<TextView>(R.id.fullName)
+        nickName = findViewById<TextView>(R.id.nickName)
+        dateOfBirth = findViewById<TextView>(R.id.dateOfBirth)
+        email = findViewById<TextView>(R.id.email)
+        phoneNumber = findViewById<TextView>(R.id.phoneNumber)
+        location = findViewById<TextView>(R.id.location)
+        image = findViewById<ImageView>(R.id.imageView3)
+
+
+        fullName.text = "Heydarli Atabay"
+        nickName.text = "Atash"
+        dateOfBirth.text = "23/09/95"
+        email.text = "heydarli.atabay@gmail.com"
+        phoneNumber.text = "345678909"
+        location.text = "Turin, Italy"
+
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.edit_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.editButton -> {
+                val intent = Intent(this, EditProfileActivity::class.java)
+                startActivityForResult(intent, 1)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
+
+
+
+
+
+
 }

3b019499de8f532ae1d026a132c85f6214a208e3
francescostag <s278937@studenti.polito.it>
2021-04-28 16:22:45 +0200

    Names updated

--- app/src/main/java/com/example/madproject/lib/Enums.kt
+++ app/src/main/java/com/example/madproject/lib/Enums.kt
@@ -3,8 +3,7 @@ package com.example.madproject.lib
 enum class Requests(val value: Int)
 {
     INTENT_CAPTURE_PHOTO(1),
-    INTENT_PHOTO_FROM_GALLERY(2),
-    INTENT_EDIT_ACTIVITY(3);
+    INTENT_PHOTO_FROM_GALLERY(2);
 
     companion object {
         fun from(findValue: Int): Requests = Requests.values().first { it.value == findValue }
@@ -12,13 +11,13 @@ enum class Requests(val value: Int)
 }
 
 enum class ValueIds(val value: String) {
-    FULL_NAME("group11.lab1.FULL_NAME"),
-    NICKNAME("group11.lab1.NICKNAME"),
-    EMAIL("group11.lab1.EMAIL"),
-    LOCATION("group11.lab1.LOCATION"),
-    PHONE_NUMBER("group11.lab1.PHONE_NUMBER"),
-    DATE_OF_BIRTH("group11.lab1.DATE_OF_BIRTH"),
-    CURRENT_PHOTO_PATH("group11.lab1.CURRENT_PHOTO_PATH"),
+    FULL_NAME("group11.lab2.FULL_NAME"),
+    NICKNAME("group11.lab2.NICKNAME"),
+    EMAIL("group11.lab2.EMAIL"),
+    LOCATION("group11.lab2.LOCATION"),
+    PHONE_NUMBER("group11.lab2.PHONE_NUMBER"),
+    DATE_OF_BIRTH("group11.lab2.DATE_OF_BIRTH"),
+    CURRENT_PHOTO_PATH("group11.lab2.CURRENT_PHOTO_PATH"),
     TRIP_DEPARTURE("group11.lab2.TRIP_DEPARTURE"),
     TRIP_ARRIVAL("group11.lab2.TRIP_ARRIVAL"),
     TRIP_DATE("group11.lab2.TRIP_DATE"),
